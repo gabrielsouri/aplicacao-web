ae9e59b112bac74f4ff6e78c9f9c093b259497bf
gabrielsouri <gabriel.souri.oliveira@gmail.com>
2025-02-20 17:35:29 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html",titulo='HOME')
+
+
+@app.route('/config')
+def config():
+    menu_op = ["Usuarios","Veiculos","Patrimonio"]
+    return render_template("config.html",menu=menu_op)
+
+@app.route('/feio')
+def foto():
+    return render_template("foto.html")
+
+
+#app.run(debug=True)
