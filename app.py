a84b9698bb5be44452b734d12ba63a13733f2e3b
gabrielsouri <gabriel.souri.oliveira@gmail.com>
2025-02-20 18:21:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
-from flask import render_template
+import os
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.abspath('templates'))
 
 @app.route('/')
 def home():
